43a0eea67416df3b454ece508e3652ef09332c17
tomasperezponisio <t.perezponisio@gmail.com>
2022-04-28 16:25:26 -0300

    entrega tp2

---
+++ TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        // constructor
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) : base (chasis, marca, color)
+        {
+        }
+
+        // propiedades
+
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
