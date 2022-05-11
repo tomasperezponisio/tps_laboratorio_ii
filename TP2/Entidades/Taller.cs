ebc02f59dd5916f5015e4319fa9c1873a547eb04
tomasperezponisio <t.perezponisio@gmail.com>
2022-05-10 21:45:22 -0300

    Correccion TP2

--- TP2/Entidades/Taller.cs
+++ TP2/Entidades/Taller.cs
@@ -36,7 +36,7 @@ namespace Entidades
         /// <returns></returns>
         public override string ToString()
         {
-            return this.Listar(this, ETipo.Todos);
+            return Listar(this, ETipo.Todos);
         }
         #endregion
 
@@ -49,7 +49,7 @@ namespace Entidades
         /// <param name="taller">Elemento a exponer</param>
         /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
         /// <returns></returns>
-        public string Listar(Taller taller, ETipo tipo)
+        public static string Listar(Taller taller, ETipo tipo)
         {
             StringBuilder sb = new StringBuilder();
 
--- TP2/TP-02/Program.cs
+++ TP2/TP-02/Program.cs
@@ -58,19 +58,19 @@ namespace TP_02_2018
             taller += c2;
 
             // Muestro solo Moto
-            Console.WriteLine(taller.Listar(taller, Taller.ETipo.Ciclomotor));
+            Console.WriteLine(Taller.Listar(taller, Taller.ETipo.Ciclomotor));
             Console.WriteLine("<-----------PRESIONE UNA TECLA PARA CONTINUAR----------->");
             Console.ReadKey();
             Console.Clear();
 
             // Muestro solo Automovil
-            Console.WriteLine(taller.Listar(taller, Taller.ETipo.Sedan));
+            Console.WriteLine(Taller.Listar(taller, Taller.ETipo.Sedan));
             Console.WriteLine("<-----------PRESIONE UNA TECLA PARA CONTINUAR----------->");
             Console.ReadKey();
             Console.Clear();
 
             // Muestro solo Camioneta
-            Console.WriteLine(taller.Listar(taller, Taller.ETipo.SUV));
+            Console.WriteLine(Taller.Listar(taller, Taller.ETipo.SUV));
             Console.WriteLine("<-------------PRESIONE UNA TECLA PARA SALIR------------->");
             Console.ReadKey();
         }
